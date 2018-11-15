985c8c323430f12e810b69840bd013c3a9821df8
luhanlin <47901673@qq.com>
2018-11-15 15:39:13 +0800

    Mr_lu 提交代码 strategy 设计模式

--- src/main/java/com/luhanlin/designpattern/strategy/practice/FastSort.java
+++ src/main/java/com/luhanlin/designpattern/strategy/practice/FastSort.java
@@ -1,17 +1,72 @@
 package com.luhanlin.designpattern.strategy.practice;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
 /**
- * 类详细描述：
+ * 类详细描述：快速排序算法,此处有点问题，后面修改
  *
  * @author Mr_lu
  * @version 1.0
  * @mail allen_lu_hh@163.com
  * 创建时间：2018/11/15 11:40 AM
  */
+@AllArgsConstructor
+@NoArgsConstructor
 public class FastSort implements Sort{
+
+    private int left;
+    private int right;
 
     @Override
     public void sort(Comparable[] data) {
 
+        quickSort(data, left, right);
+    }
+
+    private void quickSort(Comparable[] data, int left, int right) {
+        int i = left;
+        int j = right;
+
+        // 定义基础值
+        Comparable base = data[left];
+
+        if (i > j){
+            return;
+        }
+
+        while (i != j){
+            // data[j] 值比base大进入循环,从后往前比较
+            System.out.println(base.compareTo(data[j]));
+            while (i < j && base.compareTo(data[j]) <= 0){
+                j--;
+                // data[j] 值比base小进行比较交换
+                if (base.compareTo(data[j]) > 0){
+                    // 比较交换
+                    swap(data, j, i);
+                }
+            }
+            // data[i] 值比base小进入循环,从前往后比较
+            while (i < j && base.compareTo(data[i]) >= 0 ){
+                i++;
+                if (base.compareTo(data[i]) < 0){
+                    // 比较交换
+                    swap(data, i, j);
+                }
+            }
+        }
+
+        // 比较完毕后 left与right 值应该相等
+//        data[i] = base;
+        // 进行左边比较
+        if (left < i) quickSort(data, left, i-1);
+        // 进行右边比较
+        if (j < right) quickSort(data, j + 1, right);
+    }
+
+    private void swap(Comparable[] data, int i, int j) {
+        Comparable base = data[i];
+        data[i] = data[j];
+        data[j] = base;
     }
 }
--- src/main/java/com/luhanlin/designpattern/strategy/practice/Main.java
+++ src/main/java/com/luhanlin/designpattern/strategy/practice/Main.java
@@ -1,5 +1,8 @@
 package com.luhanlin.designpattern.strategy.practice;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
 /**
  * 类详细描述：
  *
@@ -12,11 +15,14 @@ public class Main {
     public static void main(String[] args) {
 
         String[] data = new String[]{
-                "Tom","John","Gaga","Allen",
+                "Tom","Gaga","John","Allen",
         };
 
-        SortAndPrint sortAndPrint = new SortAndPrint(data,new SelectSort());
-        sortAndPrint.execute();
-
+        SortAndPrint sortAndPrint01 = new SortAndPrint(data,new SelectSort());
+        SortAndPrint sortAndPrint02 = new SortAndPrint(data,new FastSort(0,data.length-1));
+//        sortAndPrint01.execute();
+        LocalDateTime now2 = LocalDateTime.now();
+        sortAndPrint02.execute();
+        System.out.println("FastSort consume time: " + ChronoUnit.MILLIS.between(now2,LocalDateTime.now()));
     }
 }
